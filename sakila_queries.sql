a48e4bf96f870e4dfee8efa31797fb032a562845
carmenlnr <c.linares@hotmail.es>
2026-04-28 16:24:31 +0200

    Update sakila_queries.sql

--- sakila_queries.sql
+++ sakila_queries.sql
@@ -1,48 +1,68 @@
 USE sakila;
+
+-- 1.  Mostrar tablas disponibles
 SHOW TABLES;
 
+-- 2. Datos completos de actor, film y customer
 SELECT * FROM actor;
 SELECT * FROM film;
 SELECT * FROM customer;
 
-SELECT title FROM film; 
+-- 3. Retrieve the following columns from their respective tables:
+-- 3.1 Títulos de todas las películas
+SELECT title 
+FROM film; 
 
+-- 3.2 Lista de idiomas
 SELECT name AS language 
 FROM language;
 
-SELECT first_name FROM staff;
-
-SELECT DISTINCT release_year 
-FROM film
-ORDER BY release_year;
-
-SELECT COUNT(store_id) AS number_of_stores 
-FROM store;
-SELECT COUNT(staff_id) AS number_of_employees 
+-- 3.3 Nombres de los empleados
+SELECT first_name 
 FROM staff;
 
+-- 4. Años de lanzamiento únicos
+SELECT DISTINCT release_year 
+FROM film;
+
+-- 5. Counting records for database insights:
+-- 5.1 Numero de tiendas
+SELECT COUNT(DISTINCT store_id) AS number_of_stores 
+FROM store;
+
+-- 5.2 Número de empleados
+SELECT COUNT(DISTINCT staff_id) AS number_of_employees 
+FROM staff;
+
+-- 5.3 Películas disponibles y alquiladas
 SELECT COUNT(DISTINCT film_id) AS films_available 
 FROM film;
 
-SELECT COUNT(DISTINCT inventory_id) AS films_rented 
+SELECT COUNT(*) AS films_rented 
 FROM rental;
 
-SELECT COUNT(DISTINCT last_name) AS distinct_last_names FROM actor;
+-- 5.4 Apellidos distintos de actores
+SELECT COUNT(DISTINCT last_name) AS distinct_last_names 
+FROM actor;
 
-SELECT title, length FROM film
+-- 6. Top 10 películas más largas
+SELECT title 
+FROM film
 ORDER BY length DESC
 LIMIT 10;
 
-SELECT * 
-FROM actor
+-- 7. Use filtering tecniques
+-- 7.1 Actores llamados SCARLETT
+SELECT * FROM actor
 WHERE first_name = 'SCARLETT';
 
+-- 7.2 Películas con ARMAGEDDON en el título y más de 100 minutos
 SELECT title, length 
 FROM film
 WHERE title LIKE '%ARMAGEDDON%'
 AND length > 100;
 
+-- 7.3 Películas con contenido Behind the Scenes
 SELECT COUNT(*) AS behind_the_scenes 
 FROM film
 WHERE special_features LIKE '%Behind the Scenes%';
-
