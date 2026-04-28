fb66f93706f0ed7e5c388b26ff2406d383e973f4
carmenlnr <c.linares@hotmail.es>
2026-04-28 15:08:38 +0200

    Update sakila_queries.sql

--- sakila_queries.sql
+++ sakila_queries.sql
@@ -5,20 +5,28 @@ SELECT * FROM actor;
 SELECT * FROM film;
 SELECT * FROM customer;
 
-SELECT title FROM film 
+SELECT title FROM film; 
 
-SELECT name AS language FROM language
+SELECT name AS language 
+FROM language;
 
 SELECT first_name FROM staff;
 
-SELECT DISTINCT release_year FROM film;
+SELECT DISTINCT release_year 
+FROM film
+ORDER BY release_year;
 
-SELECT COUNT(*) AS number_of_stores FROM store;
+SELECT COUNT(store_id) AS number_of_stores 
+FROM store;
 
-SELECT COUNT(*) AS number_of_employees FROM staff;
+SELECT COUNT(staff_id) AS number_of_employees 
+FROM staff;
 
-SELECT COUNT(*) AS films_available FROM film;
-SELECT COUNT(*) AS films_rented FROM rental;
+SELECT COUNT(film_id) AS films_available 
+FROM film;
+
+SELECT COUNT(rental_id) AS films_rented 
+FROM rental;
 
 SELECT COUNT(DISTINCT last_name) AS distinct_last_names FROM actor;
 
@@ -26,13 +34,16 @@ SELECT title, length FROM film
 ORDER BY length DESC
 LIMIT 10;
 
-SELECT * FROM actor
+SELECT * 
+FROM actor
 WHERE first_name = 'SCARLETT';
 
-SELECT title, length FROM film
+SELECT title, length 
+FROM film
 WHERE title LIKE '%ARMAGEDDON%'
-AND length > 100;
+ AND length > 100;
 
-SELECT COUNT(*) AS behind_the_scenes FROM film
+SELECT COUNT(*) AS behind_the_scenes 
+FROM film
 WHERE special_features LIKE '%Behind the Scenes%';
 
